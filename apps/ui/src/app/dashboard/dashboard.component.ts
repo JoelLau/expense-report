fddb22bc9e09cf4abf0e9320670456260cc544ee
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-02-26 14:46:06 +0800

    added sample table

--- apps/ui/src/app/dashboard/dashboard.component.ts
+++ apps/ui/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { ColDef, ColGroupDef } from 'ag-grid-community';
+import {
+  FirstDataRenderedEvent,
+  GridOptions,
+  GridReadyEvent,
+  GridSizeChangedEvent,
+  ModelUpdatedEvent,
+  ValueFormatterParams,
+} from 'ag-grid-community';
 
 @Component({
   selector: 'expense-report-dashboard',
@@ -7,19 +14,85 @@ import { ColDef, ColGroupDef } from 'ag-grid-community';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-  columnDefs: (ColDef | ColGroupDef)[] = [
-    { field: 'asdf' },
-    { field: 'qwer' },
-    { field: 'zxcv' },
-  ];
-
-  rowData = [
-    { asdf: 'aaaa', qwer: 'aaaa', zxcv: 'aaaa' },
-    { asdf: 'bbbb', qwer: 'bbbb', zxcv: 'bbbb' },
-    { asdf: 'cccc', qwer: 'cccc', zxcv: 'cccc' },
-  ];
-
-  onButtonClick(event: MouseEvent): void {
-    console.log(event);
-  }
+  gridOptions: GridOptions = {
+    columnDefs: [
+      { headerName: 'Date', field: 'date', type: 'dateColumn' },
+      {
+        headerName: 'Shop Name',
+        field: 'shopName',
+        type: 'textColumn',
+      },
+      {
+        headerName: 'Amount',
+        field: 'amount',
+        type: 'currencyColumn',
+      },
+      {
+        headerName: 'Currency',
+        field: 'currency',
+        type: 'textColumn',
+      },
+      {
+        headerName: 'Category',
+        field: 'category',
+        type: 'textColumn',
+      },
+    ],
+    rowData: [
+      {
+        date: '2022-01-01',
+        shopName: 'example shop 1',
+        amount: 1111,
+        currency: 'SGD',
+        category: 'Eating Out',
+      },
+      {
+        date: '2022-01-01',
+        shopName: 'example shop 2',
+        amount: 2222,
+        currency: 'SGD',
+        category: 'Eating Out',
+      },
+      {
+        date: '2022-01-01',
+        shopName: 'example shop 3',
+        amount: 3333,
+        currency: 'SGD',
+        category: 'Eating Out',
+      },
+    ],
+    defaultColDef: {
+      filter: true,
+      sortable: true,
+      resizable: true,
+    },
+    columnTypes: {
+      dateColumn: {
+        filter: 'agDateColumnFilter',
+      },
+      textColumn: {
+        filter: 'agTextColumnFilter',
+      },
+      currencyColumn: {
+        headerClass: 'ag-right-aligned-header',
+        cellClass: 'ag-right-aligned-cell',
+        filter: 'agNumberColumnFilter',
+        valueFormatter: ({ value }: ValueFormatterParams): string => {
+          return `${(value as number).toFixed(2)}`;
+        },
+      },
+    },
+    onFirstDataRendered: ({ api }: FirstDataRenderedEvent): void => {
+      api.sizeColumnsToFit();
+    },
+    onGridReady: ({ api }: GridReadyEvent): void => {
+      api.sizeColumnsToFit();
+    },
+    onGridSizeChanged: ({ api }: GridSizeChangedEvent): void => {
+      api.sizeColumnsToFit();
+    },
+    onModelUpdated: ({ api }: ModelUpdatedEvent): void => {
+      api.sizeColumnsToFit();
+    },
+  };
 }
