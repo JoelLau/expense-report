13efe4d68436d83b13441fe37718b59c0c85895b
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-03-19 00:11:55 +0800

    updated linting config for imports

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Message } from '@expense-report/api-interfaces';
 import { Controller, Get } from '@nestjs/common';
-
 import { AppService } from './app.service';
 
 @Controller()
--- apps/dashboard/src/app/app.component.spec.ts
+++ apps/dashboard/src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, TestBed } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
