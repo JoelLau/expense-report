1f94fd40ff898472cf7ad5f7a489fb7fb48316fa
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-03-28 15:47:03 +0800

    lint everything

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-
-import { Message } from '@expense-report/api-interfaces';
-
 import { AppService } from './app.service';
+import { Message } from '@expense-report/api-interfaces';
 
 @Controller()
 export class AppController {
