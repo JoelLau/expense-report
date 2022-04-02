445e78f45a1e70f7b4c44b177e633b3e937c619a
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-02 23:36:03 +0800

    service given-when-then format, CRUD order

--- libs/expenses/api/src/lib/expenses-api.service.spec.ts
+++ libs/expenses/api/src/lib/expenses-api.service.spec.ts
@@ -3,7 +3,7 @@ import { ExpensesApiService } from './expenses-api.service';
 import { SharedApiPrismaService } from '@expense-report/shared/api/prisma';
 import { expenseInputs } from '@expense-report/expenses/shared';
 
-describe('ExpensesApiService', () => {
+describe(`given 'ExpensesApiService()'`, () => {
   let service: ExpensesApiService;
   let prisma: SharedApiPrismaService;
 
@@ -34,29 +34,37 @@ describe('ExpensesApiService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('getExpenses', () => {
-    it('should call prisma.expense.findMany', async () => {
-      await service.getExpenses();
-      expect(prisma.expense.findMany).toHaveBeenCalled();
+  describe(`given 'createExpenses()'`, () => {
+    describe('when called', () => {
+      it(`should call prisma.expense.create`, async () => {
+        await service.createExpenses(expenseInputs);
+        expect(prisma.expense.create).toHaveBeenCalledTimes(
+          expenseInputs.length
+        );
+      });
+
+      it(`should call prisma.$transaction`, async () => {
+        await service.createExpenses(expenseInputs);
+        expect(prisma.$transaction).toHaveBeenCalled();
+      });
     });
   });
 
-  describe('getExpense', () => {
-    it('should call prisma.expense.findUnique', async () => {
-      await service.getExpense('5');
-      expect(prisma.expense.findUnique).toHaveBeenCalled();
+  describe(`given 'getExpenses()'`, () => {
+    describe('when called', () => {
+      it('should call prisma.expense.findMany', async () => {
+        await service.getExpenses();
+        expect(prisma.expense.findMany).toHaveBeenCalled();
+      });
     });
   });
 
-  describe('createExpenses', () => {
-    it(`should call prisma.expense.create`, async () => {
-      await service.createExpenses(expenseInputs);
-      expect(prisma.expense.create).toHaveBeenCalledTimes(expenseInputs.length);
-    });
-
-    it(`should call prisma.$transaction`, async () => {
-      await service.createExpenses(expenseInputs);
-      expect(prisma.$transaction).toHaveBeenCalled();
+  describe(`given 'getExpense()'`, () => {
+    describe('when called', () => {
+      it('should call prisma.expense.findUnique', async () => {
+        await service.getExpense('5');
+        expect(prisma.expense.findUnique).toHaveBeenCalled();
+      });
     });
   });
 });
