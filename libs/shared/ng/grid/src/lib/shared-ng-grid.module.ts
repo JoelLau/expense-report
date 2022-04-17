cce71a65a7fb9fba979acb16c5ac6f91142d9ce6
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-17 17:57:47 +0800

    use shared-ui button in cell-renderer

--- libs/shared/ng/grid/src/lib/shared-ng-grid.module.ts
+++ libs/shared/ng/grid/src/lib/shared-ng-grid.module.ts
@@ -2,9 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
 import { ActionsComponent } from './cell-renderer/actions/actions.component';
+import { ButtonComponentModule } from '@expense-report/shared/ui';
 
 @NgModule({
-  imports: [CommonModule, AgGridModule.withComponents([ActionsComponent])],
+  imports: [
+    CommonModule,
+    AgGridModule.withComponents([ActionsComponent]),
+    ButtonComponentModule,
+  ],
   declarations: [ActionsComponent],
   exports: [AgGridModule, ActionsComponent],
 })
