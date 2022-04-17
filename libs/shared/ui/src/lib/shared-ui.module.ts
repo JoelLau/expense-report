1aed6b55d708f4d04c90c1a3dc747e76ec415d29
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-17 17:50:34 +0800

    basic button component

--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponentModule } from './button/button.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponentModule],
+  exports: [ButtonComponentModule],
 })
 export class SharedUiModule {}
