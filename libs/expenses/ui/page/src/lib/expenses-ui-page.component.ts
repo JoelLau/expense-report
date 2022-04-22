bc7995f61c7c31b1e5fcbd9b97dfc69f5bb9f2ea
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-22 17:22:18 +0800

    gradient background

--- libs/expenses/ui/page/src/lib/expenses-ui-page.component.ts
+++ libs/expenses/ui/page/src/lib/expenses-ui-page.component.ts
@@ -1,19 +1,22 @@
-import { ChangeDetectorRef, Component, OnChanges } from '@angular/core';
-import {
-  BehaviorSubject,
-  delay,
-  finalize,
-  map,
-  Observable,
-  switchMap,
-  tap,
-} from 'rxjs';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { BehaviorSubject, map, Observable, switchMap, tap } from 'rxjs';
 import { ExpensesDataAccessService } from '@expense-report/expenses/data-access';
 import { Expense } from '@expense-report/expenses/shared';
 
 @Component({
   selector: 'expense-report-expenses-ui-page',
   templateUrl: './expenses-ui-page.component.html',
+  // styles: [
+  //   `
+  //     :ng-host {
+  //       background-color: #2c539e;
+  //       @include filter-gradient(#2c539e, #ffffff, vertical);
+  //       @include background-image(
+  //         linear-gradient(top, #2c539e 50%, #ffffff 50%)
+  //       );
+  //     }
+  //   `,
+  // ],
 })
 export class ExpensesUiPageComponent {
   isFetchingExpenses = false;
