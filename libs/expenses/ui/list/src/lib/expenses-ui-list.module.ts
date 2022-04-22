a20024b398a98a6d934ec30773dd1badad52bddd
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-22 19:35:42 +0800

    list-item component

--- libs/expenses/ui/list/src/lib/expenses-ui-list.module.ts
+++ libs/expenses/ui/list/src/lib/expenses-ui-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ExpensesUiListItemComponent } from './expenses-ui-list-item.component';
 import { ExpensesUiListComponent } from './expenses-ui-list.component';
 import { SharedNgCommonModule } from '@expense-report/shared/ng/common';
 
 @NgModule({
   imports: [SharedNgCommonModule],
-  declarations: [ExpensesUiListComponent],
+  declarations: [ExpensesUiListComponent, ExpensesUiListItemComponent],
   exports: [ExpensesUiListComponent],
 })
 export class ExpensesUiListModule {}
